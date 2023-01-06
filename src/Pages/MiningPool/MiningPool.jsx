676a58199554af337a653f3e82c2032762f34ad2
Adiyi Xavier <40220028+xavierlamar@users.noreply.github.com>
2023-01-06 12:02:49 +0100

    x

--- src/Pages/MiningPool/MiningPool.jsx
+++ src/Pages/MiningPool/MiningPool.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import './MiningPool.css'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
